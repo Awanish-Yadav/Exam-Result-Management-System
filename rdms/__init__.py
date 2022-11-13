653116e2dac69738e335dde843172f47ca60e5bf
Awanish-Yadav <72813633+Awanish-Yadav@users.noreply.github.com>
2022-11-13 17:40:03 +0530

    Create __init__.py

---
+++ rdms/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from datetime import timedelta
+
+
+# Initialize flask app
+app = Flask(__name__) 
+
+app.secret_key = '12345'
+
+# Configure database connection
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1234@localhost/ers'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+app.config['REMEMBER_COOKIE_DURATION'] = timedelta(seconds=180)
+app.config['FLASK_ADMIN_FLUID_LAYOUT'] = True
+
+db = SQLAlchemy(app)
+
+
+from rdms import routes
+with app.app_context():
+    db.create_all()
